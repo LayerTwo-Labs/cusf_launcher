8d898096f92e1cde76b64bba98a192cb97b81dd1
CryptAxe <cryptaxe@gmail.com>
2024-10-23 13:08:21 -0700

    Display setup status on main window

--- cusf_launcher/script/main_window.gd
+++ cusf_launcher/script/main_window.gd
@@ -10,8 +10,7 @@ var started_pid = []
 
 
 func _ready() -> void:
-	call_deferred("update_resource_status")
-	call_deferred("update_configuration_status")
+	call_deferred("update_l1_resource_status")
 
 
 func _exit_tree() -> void:
@@ -22,117 +21,159 @@ func kill_started_pid() -> void:
 	for pid in started_pid:
 		print("Killing pid ", pid)
 		OS.kill(pid)
-
-
-func _on_button_download_cusf_pressed() -> void:
-	$ProgressDownload.visible = true
-	
-	# Download everything required to run CUSF mainchain
-	$ResourceDownloader.download_grpcurl()
-	$ResourceDownloader.download_enforcer()
-	$ResourceDownloader.download_bitcoin()
-	$ResourceDownloader.download_thunder()
-
-
-func _on_button_configure_pressed() -> void:
-	$Configuration.write_bitcoin_configuration()
-
-
-func _on_button_start_cusf_pressed() -> void:
-	var user_dir : String = OS.get_user_data_dir() 
-	var ret : int = OS.create_process(str(user_dir, "/L1-bitcoin-patched-latest-x86_64-unknown-linux-gnu/qt/bitcoin-qt"), ["--connect=0", "--signet"])
-	if ret == -1:
-		printerr("Failed to start bitcoin")
-	else:
-		started_pid.push_back(ret)
-		print("started bitcoin with pid: ", ret)
 		
-	ret = OS.create_process(str(user_dir, "/bip300301-enforcer-latest-x86_64-unknown-linux-gnu/bip300301_enforcer-0.1.0-x86_64-unknown-linux-gnu"), ["--node-rpc-addr=127.0.01:38332", "--node-rpc-user=user", "--node-rpc-pass=password", "--node-zmq-addr-sequence=tcp://0.0.0.0:29000"])
-	if ret == -1:
-		printerr("Failed to start enforcer")
-	else:
-		started_pid.push_back(ret)
-		print("started enforcer with pid: ", ret)
-
-			
-	ret = OS.create_process(str(user_dir, "/thunder-latest-x86_64-unknown-linux-gnu"), [""])
-	if ret == -1:
-		printerr("Failed to start enforcer")
-	else:
-		started_pid.push_back(ret)
-		print("started thunder with pid: ", ret)
-
-
 
 func _on_downloader_resource_bitcoin_ready() -> void:
 	resource_bitcoin_ready = true
-	update_resource_status()
+	update_l1_resource_status()
 
 
 func _on_downloader_resource_enforcer_ready() -> void:
 	resource_enforcer_ready = true
-	update_resource_status()
+	update_l1_resource_status()
 	
 
 func _on_downloader_resource_grpcurl_ready() -> void:
 	resource_grpcurl_ready = true
-	update_resource_status()
+	update_l1_resource_status()
 	
 
 func _on_resource_downloader_resource_thunder_ready() -> void:
 	resource_thunder_ready = true
-	update_resource_status()
+	update_thunder_resource_status()
 
 	
 func _on_configuration_complete() -> void:
 	configuration_complete = true
-	update_configuration_status()
 
 
-func update_resource_status() -> void:
-	check_ready_to_launch()
+func update_l1_resource_status() -> void:
+	# Hide setup status and buttons if everything L1 is ready
+	# Show launch L1 button
+	if resource_bitcoin_ready && resource_grpcurl_ready && resource_enforcer_ready:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/LabelL1SetupStatus.visible = false
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/L1ProgressBar.visible = false
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/ButtonSetupL1.visible = false
 		
-	var resource_label : String = "Resources:\n"
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/ButtonStartL1.visible = true
+		
+		return
+	
+	# Otherwise show progress of getting L1 ready
+	var status_text : String = ""
+	
 	if resource_bitcoin_ready:
-		resource_label += "\nBitcoin: Ready"
+		status_text += "\nBitcoin: Ready!"
 	else:
-		resource_label += "\nBitcoin: Missing"
+		status_text += "\nBitcoin: Downloading..."
 		
 	if resource_grpcurl_ready:
-		resource_label += "\nGRPCURL: Ready"
+		status_text += "\nGRPCURL: Ready!"
 	else:
-		resource_label += "\nGRPCURL: Missing"
+		status_text += "\nGRPCURL: Downloading..."
 		
 	if resource_enforcer_ready:
-		resource_label += "\nEnforcer: Ready"
+		status_text += "\nEnforcer: Ready!"
 	else:
-		resource_label += "\nEnforcer: Missing"
+		status_text += "\nEnforcer: Downloading..."
 	
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/LabelL1SetupStatus.text = status_text
+
+
+func update_thunder_resource_status() -> void:
 	if resource_thunder_ready:
-		resource_label += "\nThunder: Ready"
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/ButtonSetupThunder.visible = false
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/ThunderProgressBar.visible = false
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/LabelThunderSetupStatus.visible = false
+
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/ButtonStartThunder.visible = true
+
+
+func _on_button_overview_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage.visible = true
 	else:
-		resource_label += "\nThunder: Missing"
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage.visible = false
+
+
+func _on_button_wallets_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/WalletPage.visible = true
+	else:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/WalletPage.visible = false
+
+
+func _on_button_tools_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/ToolsPage.visible = true
+	else:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/ToolsPage.visible = false
+
+
+func _on_button_settings_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/SettingPage.visible = true
+	else:
+		$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/SettingPage.visible = false
+
+
+func _on_button_setup_l1_pressed() -> void:
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/ButtonSetupL1.visible = false
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/L1ProgressBar.visible = true
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/LabelL1SetupStatus.visible = true
 	
-	$LabelResources.text = resource_label
+	# Download everything required to run L1
+	$ResourceDownloader.download_grpcurl()
+	$ResourceDownloader.download_enforcer()
+	$ResourceDownloader.download_bitcoin()
+	
+	# Configure L1 software
+	$Configuration.write_bitcoin_configuration()
 
 
-func update_configuration_status() -> void:
-	check_ready_to_launch()
+func _on_button_start_l1_pressed() -> void:
+	var user_dir : String = OS.get_user_data_dir() 
+	
+	# Start bitcoin
+	var ret : int = OS.create_process(str(user_dir, "/L1-bitcoin-patched-latest-x86_64-unknown-linux-gnu/qt/bitcoin-qt"), ["--connect=0", "--signet"])
+	if ret == -1:
+		printerr("Failed to start bitcoin")
+		return
+	else:
+		started_pid.push_back(ret)
+		print("started bitcoin with pid: ", ret)
+	
+	# Start bip300-301 enforcer
+	ret = OS.create_process(str(user_dir, "/bip300301-enforcer-latest-x86_64-unknown-linux-gnu/bip300301_enforcer-0.1.0-x86_64-unknown-linux-gnu"), ["--node-rpc-addr=127.0.01:38332", "--node-rpc-user=user", "--node-rpc-pass=password", "--node-zmq-addr-sequence=tcp://0.0.0.0:29000"])
+	if ret == -1:
+		printerr("Failed to start enforcer")
+		return
+	else:
+		started_pid.push_back(ret)
+		print("started enforcer with pid: ", ret)
 		
-	var config_label : String = "Configuration:\n"
-	if configuration_complete:
-		config_label += "\nBitcoin: configured"
-	else:
-		config_label += "\nBitcoin: Not configured"
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL1/VBoxContainer/ButtonStartL1.visible = false
 
-	$LabelConfiguration.text = config_label
+
+func _on_button_setup_thunder_pressed() -> void:
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/ButtonSetupThunder.visible = false
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/ThunderProgressBar.visible = true
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/LabelThunderSetupStatus.visible = true
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/LabelThunderSetupStatus.text = "Downloading Thunder..."
 	
-	
-func check_ready_to_launch() -> void:
-	if resource_bitcoin_ready && resource_enforcer_ready && resource_grpcurl_ready && resource_thunder_ready:
-		if configuration_complete:
-			$ButtonStartCUSF.disabled = false
-			
-		$ProgressDownload.visible = false
+	$ResourceDownloader.download_thunder()
+
+
+func _on_button_start_thunder_pressed() -> void:
+	var user_dir : String = OS.get_user_data_dir() 
+
+	var ret : int = OS.create_process(str(user_dir, "/thunder-latest-x86_64-unknown-linux-gnu"), [""])
+	if ret == -1:
+		printerr("Failed to start enforcer")
+		return
 	else:
-		$ButtonStartCUSF.disabled = true
+		started_pid.push_back(ret)
+		print("started thunder with pid: ", ret)
+		
+	$MarginContainer/VBoxContainer/HBoxContainerPageAndPageButtons/PanelContainerPages/OverviewPage/GridContainer/PanelContainerL2/VBoxContainer/ButtonStartThunder.visible = false
+		
